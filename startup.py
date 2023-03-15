7ab0942692a0caedfe11cf43fe879a93f731887d
Luca Deppieri <ldeppieri0516@gmail.com>
2023-03-15 08:44:46 +0100

    Add files via upload

--- startup.py
+++ startup.py
@@ -1,15 +1,10 @@
 import subprocess
+from os.path import exists
 
 # Dizioneario contenente i colori
 colors = {
-    "red": "\033[31m",
-    "green": "\033[32m",
     "yellow": "\033[33m",
-    "blue": "\033[34m",
-    "purple": "\033[35m",
     "cyan": "\033[36m",
-    "white": "\033[37m",
-    "black": "\033[38m",
     "reset": "\033[0m"
 }
 
@@ -21,28 +16,30 @@ subprocess.run(["sudo", "apt", "upgrade", "-y"])
 subprocess.run(["sudo", "apt", "autoremove", "-y"])
 output = subprocess.check_output(
     ["apt", "list", "--upgradable"]).decode("utf-8")
-print(colors["cyan"] + "###  Aggiornamento completato!\n" + colors["reset"])
 
 # Se il numero di righe di output è maggiore di 1, allora ci sono pacchetti aggiornabili
-if(len(output.splitlines()) > 1):
-    with open("upgradable.txt", "r+") as file:
-        file.truncate(0)
+if (len(output.splitlines()) > 1):
+    if exists("upgradable.txt"):
+        with open("upgradable.txt", "r+") as file:
+            file.truncate(0)
     with open("upgradable.txt", "w") as file:
         for line in output.splitlines()[1:]:
             package_name = line.split("/")[0]
             file.write(package_name + "\n")
     with open("upgradable.txt", "r") as file:
         print(colors["cyan"] +
-              "###  I seguenti pacchetti sono aggiornabili:\n")
-        for line in file[1:]:
-            print(line + "\n")
-    user_input = ("Vuoi installarli? (y/n): ")
+              "###  I seguenti pacchetti sono aggiornabili:")
+        print(colors["yellow"])
+        for line in file:
+            print(line)
+        print(colors["cyan"])
+    user_input = input("Vuoi installarli? (y/n): ")
     if user_input == "y":
         print("### Installazione in corso..." + colors["reset"])
         with open("upgradable.txt", "r") as file:
             for line in file:
                 subprocess.run(["sudo", "apt", "install", "-y", line])
-        print(colors["cyan"] + "###  Installazione completata!\n")
+        print(colors["cyan"] + "\n###  Installazione completata!")
 else:
     print(colors["cyan"] + "###  Nessun pacchetto aggiuntivo da installare!")
 
