c6352a12dc5cd91c0dd4b3fbe01f258be1fc36bf
Luca Deppieri <ldeppieri0516@gmail.com>
2023-03-15 10:09:05 +0100

    Add files via upload

--- startup.py
+++ startup.py
@@ -1,57 +1,62 @@
 import subprocess
 from os.path import exists
 
-# Dizioneario contenente i colori
+# Dictionary containing the ANSI escape codes for the colors used in the script
 colors = {
     "yellow": "\033[33m",
     "cyan": "\033[36m",
     "reset": "\033[0m"
 }
 
-print(colors["cyan"] + "\n###  Aggiornamento in corso..." + colors["reset"])
 
-# Esegui il comando "sudo apt update", "sudo apt upgrade -y", "sudo apt autoremove -y" e "apt list --upgradable"
-subprocess.run(["sudo", "apt", "update"])
-subprocess.run(["sudo", "apt", "upgrade", "-y"])
-subprocess.run(["sudo", "apt", "autoremove", "-y"])
-output = subprocess.check_output(
-    ["apt", "list", "--upgradable"]).decode("utf-8")
+def upgrade():
+    # Run the command "sudo apt-get update" and save the output in the variable "output"
+    print(colors["cyan"] + "\n>  Searching for updates..." + colors["reset"])
+    subprocess.run(["sudo", "apt-get", "update", "|", "grep", "-q"])
 
-# Se il numero di righe di output è maggiore di 1, allora ci sono pacchetti aggiornabili
-if (len(output.splitlines()) > 1):
-    if exists("upgradable.txt"):
-        with open("upgradable.txt", "r+") as file:
-            file.truncate(0)
-    with open("upgradable.txt", "w") as file:
+    # Run the command "sudo apt-get upgrade -y" and save the output in the variable "output"
+    print(colors["cyan"] + "\n>  Upgrading..." + colors["reset"])
+    subprocess.run(["sudo", "apt-get", "upgrade", "-y"])
+    subprocess.run(["sudo", "apt-get", "autoremove", "-y"])
+    output = subprocess.check_output(
+        ["apt", "list", "--upgradable"]).decode("utf-8")
+
+    # If the number of output lines is greater than 1, then there are upgradable packages
+    if (len(output.splitlines()) > 1):
+        packages_to_upgrade = ""
         for line in output.splitlines()[1:]:
             package_name = line.split("/")[0]
-            file.write(package_name + "\n")
-    with open("upgradable.txt", "r") as file:
-        print(colors["cyan"] +
-              "###  I seguenti pacchetti sono aggiornabili:")
-        print(colors["yellow"])
-        for line in file:
-            print(line)
-        print(colors["cyan"])
-    user_input = input("Vuoi installarli? (y/n): ")
+            packages_to_upgrade += package_name + "\n"
+        print(colors["cyan"] + ">  The following packages can be upgraded:")
+        print(colors["yellow"] + packages_to_upgrade + colors["cyan"])
+        user_input = input("Do you want to install them? (y/n): ")
+        if user_input == "y":
+            print("> Upgrading..." + colors["reset"])
+            for package_name in packages_to_upgrade.splitlines():
+                if package_name:
+                    subprocess.run(
+                        ["sudo", "apt-get", "install", "-y", package_name])
+            print(colors["cyan"] + "\n>  Upgrade completed!")
+    else:
+        print(colors["cyan"] + ">  No packages to upgrade!\n")
+    del output  # Delete the variable "output"
+
+
+def clean():
+    # Ask the user if he wants to clean the system
+    print(">  Upgrade completed!\n")
+    user_input = input("Do you want to clean the system? (y/n): ")
+
+    # If the user has entered "y", then run the commands "sudo apt-get clean" and "sudo apt-get autoremove --purge"
     if user_input == "y":
-        print("### Installazione in corso..." + colors["reset"])
-        with open("upgradable.txt", "r") as file:
-            for line in file:
-                subprocess.run(["sudo", "apt", "install", "-y", line])
-        print(colors["cyan"] + "\n###  Installazione completata!")
-else:
-    print(colors["cyan"] + "###  Nessun pacchetto aggiuntivo da installare!")
+        print("\n> Cleaning..." + colors["reset"])
+        subprocess.run(["sudo", "apt-get", "clean"])
+        subprocess.run(["sudo", "apt-get", "autoremove", "--purge"])
+        print(colors["cyan"] + "> All cleaned!\n")
 
-# Domanda all'utente se vuole ripulire il sistema
-print("###  Aggiornamento completato!\n")
-user_input = input("Vuoi pulire il sistema? (y/n): ")
+    print(colors["reset"])
 
-# Se l'utente ha inserito "y", allora esegui il comando "sudo apt clean" e "sudo apt autoremove --purge"
-if user_input == "y":
-    print("\n### Pulizia in corso..." + colors["reset"])
-    subprocess.run(["sudo", "apt", "clean"])
-    subprocess.run(["sudo", "apt", "autoremove", "--purge"])
-    print(colors["cyan"] + "### Pulizia completata!\n")
 
-print(colors["reset"])
+# Run the functions
+upgrade()
+clean()
